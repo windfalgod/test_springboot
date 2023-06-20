c98b686a1595c77ab72e8dedce11a76175e6a01f
xuannghia <nghiagod121122@gmail.com>
2023-06-20 15:18:56 +0700

    build basic project

--- src/main/java/com/example/test_springboot/TestSpringbootApplication.java
+++ src/main/java/com/example/test_springboot/TestSpringbootApplication.java
@@ -2,17 +2,18 @@ package com.example.test_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestSpringbootApplication.class, args);
-        test();
     }
 
-    public static void test() {
-        System.out.println("hello");
+    public int test(int number) {
+        return number * 10;
     }
 
 }
